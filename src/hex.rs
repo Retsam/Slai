b0b8a33c08279bcd33d60168841bf1dcf31ee49c
Ben Nelson <Ben.Nelson@inin.com>
2016-05-04 18:45:18 -0400

    Allow specific coord formats to be constructed directly

--- src/hex.rs
+++ src/hex.rs
@@ -3,10 +3,46 @@ pub struct HexCoordsAxial {
     pub row: i32,
 }
 
+impl HexCoordsAxial {
+    pub fn new(row: i32, col: i32) -> HexCoordsAxial {
+        HexCoordsAxial {
+            row: row,
+            col: col
+        }
+    }
+}
+
 pub struct HexCoordsCube {
     pub x: i32,
     pub y: i32,
     pub z: i32,
+}
+
+impl HexCoordsCube {
+
+    /// The cube-coordinate variant of hex coordinates
+    /// Specified by three dimensions (x, y, z) which should sum to zero.
+
+    pub fn new(x: i32, y: i32, z: i32) -> HexCoordsCube {
+        assert!(x + y + z == 0, "Invalid hex coordinates provided: ({}, {}, {}). Cube coordinates should sum to zero", x, y, z);
+        HexCoordsCube {
+            x: x,
+            y: y,
+            z: z
+        }
+    }
+
+}
+
+#[test]
+fn valid_cube_coord() {
+    HexCoordsCube::new(1,2,-3);
+}
+
+#[test]
+#[should_panic]
+fn invalid_cube_coord() {
+    HexCoordsCube::new(1,2,3);
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -52,14 +88,16 @@ impl HexCoords {
         }
     }
 
+    /// Returns an array of hex coords adjacent to the given hex coord
+
     pub fn get_neighbors(&self) -> [HexCoords; 6] {
         [
-            HexCoords::new(self.row + 1, self.col),
-            HexCoords::new(self.row + 1, self.col - 1),
             HexCoords::new(self.row, self.col + 1),
-            HexCoords::new(self.row, self.col - 1),
             HexCoords::new(self.row - 1, self.col + 1),
-            HexCoords::new(self.row - 1, self.col)
+            HexCoords::new(self.row - 1, self.col),
+            HexCoords::new(self.row, self.col - 1),
+            HexCoords::new(self.row + 1, self.col),
+            HexCoords::new(self.row + 1, self.col - 1)
         ]
     }
 }
